ecc3b9b46855ab1836625efb10d60621ccf17b98
Jayashankar Majhi <73972275+Jayashankar2@users.noreply.github.com>
2021-11-21 11:50:16 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="stylesheet/signup.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo1">
+                <span>Ap</span>erture
+            </div>
+            <ul>
+                <li><a href="about">About</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+            <a href="index.php"><button>Login</button></a>
+        </nav>
+    </header>
+    <main>
+        <div>
+            <div class="heading">
+                <span>Signup</span>
+            </div>
+            <form method="" action="" enctype="">
+                <input type="" name="username" placeholder="Username"><br/>
+                <input type="text" name="fullname" placeholder="Full Name"><br/>
+                <input type="email" name="useremail" placeholder="Email"><br/>
+                <input type="password" name="userpassword" placeholder="Password"><br/>
+                <input type="password" name="confirmpassword" placeholder="Confirm Password"><br/>
+                <button type="submit" name="signup">Signup</button>
+            </form>
+            <a href="index.html" class="return">Already have an account</a>
+        </div>
+    </main>
+    <footer>
+        <div class="col1">
+            <div class="logo">
+                <span>Ap</span>erture
+            </div>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+        <div class="col2">
+            <ul>
+                <li><a href="https://www.instagram.com/createzui/">Instagram</a></li>
+                <li><a href="https://twitter.com/jayashankar25/">Twitter</a></li>
+                <li><a href="https://in.pinterest.com/jayashankarmajhi/">Pinterest</a></li>
+                <li><a href="https://www.facebook.com/createzui/">Facebook</a></li>
+                <li><a href="https://www.youtube.com/channel/UCEtXHbAbUoXlh2n-cv9I2rw/">Youtube</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
